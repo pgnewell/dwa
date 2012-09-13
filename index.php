892bcd2275d65b422f8ef983b3669f9504907fee
paul newell <paulgnewell@gmail.com>
2012-09-13 16:09:35 -0400

    Adding junky web text

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+echo "Here is my website<br/>
+<br/>
+I am Paul Newell<br/>
+Born in Canada, grew up in Mass, live in Charlestown.<br/>
+
+a Software Engineer.<br/>
+
+This was written using vi<br/>
+
+I am using Hurricane Electric web hosting but that will probably change.<br/>"
+
+?>
