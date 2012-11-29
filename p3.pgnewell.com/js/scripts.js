b75a0814cdd63d174e3fa2b9d204ec5e090a03e1
paul newell <paulgnewell@gmail.com>
2012-11-29 00:10:35 -0500

    make the inserts more readable. fix some stuff

--- p3.pgnewell.com/js/scripts.js
+++ p3.pgnewell.com/js/scripts.js
@@ -1,6 +1,8 @@
 $(document).ready(function() { // start doc ready; do not delete this!
 
+	// first extract some static information from hidden divs 
 	var edit_buttons = $('#icon-buttons').html();
+	// only one of these buttons should be active at a time so it can have an id
 	var new_edit_buttons = edit_buttons.replace( /class=\"class/g, 'id="id' );
 
 	var after_buttons = $('#after-buttons').html();
@@ -8,38 +10,62 @@ $(document).ready(function() { // start doc ready; do not delete this!
 	// When the user clicks on a step icon it should create a dialog in the recipe 
 	// with the form included. Then turn off all the icon clickables
 	$(".icon-click").live ('click', function() {
-		debugger;
 		var new_div = $(this).clone();
 		new_div.addClass('recipe-step');
 		new_div.removeClass('icon-block icon-click');
 		new_div.find('.step-actions').html(new_edit_buttons);
-		$("#recipe-step-list").append( new_div );
+		$("#recipe-footer").before( new_div );
+		// remove before adding to avoid confusing the browser
 		$('.icon-block').removeClass('icon-click');
 		$('.icon-block').addClass('icon-error');
 		new_div.find('.step-instructions').removeClass('hide');
 		new_div.find('.step-actions').removeClass('hide');
 	});	
-	
+
+	// clicking on an icon while a step is being filled should emit an error
 	$(".icon-error").live ('click', function() {
 		$('#message-block').html('Finish this step or cancel it before adding more');
 		$('#message-block').removeClass('hide');
 	});
-	
+
 	// activate the two action buttons they should turn the icons-click back on
 	$('#id-done-button').live ('click', function() {
 		debugger;
-		console.log('I am in done');
-		$('.icon-block').addClass('icon-click');
-		$('.icon-block').removeClass('icon-error');
-		$(this).parent().html(after_buttons)
+		var box = $(this).parent().parent();
+		var step_type = box.find('img').attr('title');		
+		var instructions = box.find('textarea').val();
+		box.id = Recipe.next_id();
+		if (instructions.length == 0) {
+			$('#message-block').html('Cannot add without instructions');
+			$('#message-block').removeClass('hide');
+		} else {
+			$('#message-block').html('');
+			$('#message-block').addClass('hide');
+			$('.icon-block').removeClass('icon-error');
+			$('.icon-block').addClass('icon-click');
+			$(this).parent().html(after_buttons);
+			Recipe.add_step(step_type,box.id,instructions,[] ); 
+		};
 	});
+
+	// cancel discards the step being entered
 	$('#id-can-button').live ('click', function() {
 		console.log('I am in cancel');
 		$(this).parent().parent().remove();
+		$('#message-block').html('');
+		$('#message-block').addClass('hide');
 	});
+	
+	// delete removes an already filled out step
 	$('#id-del-button').live ('click', function() {
-		console.log('I am in cancel');
+		console.log('I am in delete');
 	});
+	
+	// execute the recipe
+	$('#execute-button').click ( function () {
+		Recipe.list_step();
+	});
+	
 	//$(".icon-block").draggable({ containment: "#recipe-block" });
 //   	$( ".draggable" ).draggable({ revert: "invalid" });
 //     $( ".droppable" ).droppable({
