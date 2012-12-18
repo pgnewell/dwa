c641337d4329d5078e149768fed6974dcf64e698
paul newell <paulgnewell@gmail.com>
2012-12-18 08:22:50 -0500

    recipe header form

--- p4/views/v_recipe_header.php
+++ p4/views/v_recipe_header.php
@@ -2,6 +2,6 @@
   Name<br>
   <input type='text' name='Recipe name'><br>
   Password:<br>
-  <textarea name='description'><br>
+	  <textarea name='description'><br>
   <input type='submit' value='Save'>
 </form>
